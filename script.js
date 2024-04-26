f55c0ee8fca2f6cac9052787133f006d2533b876
SumDudee <149608856+SumDudee@users.noreply.github.com>
2024-04-26 21:08:39 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+let scrollContainer = document.querySelector(".gallery");
+let backBtn = document.getElementById("backBtn");
+let nextBtn = document.getElementById("nextBtn");
+
+// gallery scroll
+scrollContainer.addEventListener("wheel", (evt) => {
+    evt.preventDefault();
+    scrollContainer.scrollLeft += evt.deltaY;
+});
+
+nextBtn.addEventListener("click", ()=> {
+    scrollContainer.scrollLeft += 1600;
+});
+
+backBtn.addEventListener("click", ()=> {
+    scrollContainer.scrollLeft -= 1600;
+});
+
+
+//gallery popup
+document.querySelectorAll('.gallery img').forEach(image =>{
+    image.onclick = () =>{
+        document.querySelector('.popup-img').style.display = 'block';
+        document.querySelector('.popup-img img').src = image.getAttribute('src');
+
+    }
+});
+
+document.querySelector('.popup-img span').onclick = () =>{
+    document.querySelector('.popup-img').style.display = 'none';    
+}
+
+
+
+//hamburger menu
+const hamMenu = document.querySelector(".ham-menu");
+
+const offScreenMenu = document.querySelector(".off-screen-menu");
+
+hamMenu.addEventListener("click", () => {
+  hamMenu.classList.toggle("active");
+  offScreenMenu.classList.toggle("active");
+});
