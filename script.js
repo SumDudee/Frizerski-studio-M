112e71271fb7dfe008264eb1f4d15076bcc20ed8
SumDudee <149608856+SumDudee@users.noreply.github.com>
2024-04-30 23:03:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -16,21 +16,17 @@ backBtn.addEventListener("click", ()=> {
     scrollContainer.scrollLeft -= 1600;
 });
 
-
 //gallery popup
 document.querySelectorAll('.gallery img').forEach(image =>{
     image.onclick = () =>{
         document.querySelector('.popup-img').style.display = 'block';
         document.querySelector('.popup-img img').src = image.getAttribute('src');
-
     }
 });
 
 document.querySelector('.popup-img span').onclick = () =>{
     document.querySelector('.popup-img').style.display = 'none';    
 }
-
-
 
 //hamburger menu
 const hamMenu = document.querySelector(".ham-menu");
